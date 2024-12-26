500a5ed71dc088e7ee78a4574a967bcf56ac63b1
aboutvlads <about.vlad@icloud.com>
2024-12-26 21:16:12 +0100

    v

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -5,39 +5,9 @@ export default function LandingPage() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    // Load landing page CSS
-    const link = document.createElement('link');
-    link.href = '/src/landing-page/css/style.css';
-    link.rel = 'stylesheet';
-    document.head.appendChild(link);
-
-    // Load landing page script
-    const script = document.createElement('script');
-    script.src = '/src/landing-page/js/script.js';
-    script.async = true;
-    document.body.appendChild(script);
-
-    return () => {
-      // Cleanup when component unmounts
-      document.head.removeChild(link);
-      document.body.removeChild(script);
-    };
+    // Redirect to the landing page HTML file
+    window.location.href = '/landing-page/index.html';
   }, []);
 
-  // Load the HTML content from your landing page
-  useEffect(() => {
-    fetch('/src/landing-page/index.html')
-      .then(response => response.text())
-      .then(html => {
-        // Extract the body content
-        const bodyContent = html.match(/<body[^>]*>([\s\S]*)<\/body>/i)?.[1] || '';
-        document.getElementById('landing-content')?.insertAdjacentHTML('beforeend', bodyContent);
-      });
-  }, []);
-
-  return (
-    <div id="landing-content">
-      {/* Your landing page HTML will be injected here */}
-    </div>
-  );
+  return null;
 }
